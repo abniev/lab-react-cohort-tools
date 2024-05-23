e4285c614466c82d912a1d99e8463c0019baa541
Abner Nieves <nieves.abner@gmail.com>
2024-05-23 16:49:42 -0400

    Lab Done Claudia, Abner and Yves

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import {Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
@@ -10,15 +11,16 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
+      
       <div className="pages">
-        <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-        <StudentDetailsPage />
+        <Route  path="/students/:studentsId" element={<StudentDetailsPage />} />
 
-        <UserProfilePage />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
       </div>
-
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
+import { Link, useParams, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
-
+import StudentDetailsPage from "./StudentDetailsPage";
 import studentsData from "../assets/students.json";
 
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
   const [students, setStudents] = useState(studentsData);
+  
+
 
   return (
     <div className="border-2 border-rose-500 m-2">
@@ -21,7 +24,8 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
-              <StudentCard key={student._id} {...student} />
+       <Link to={`/students/${student._id}`} > <StudentCard key={student._id} {...student} /> 
+       </Link>
           );
         })}
     </div>
